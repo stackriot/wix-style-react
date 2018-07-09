2057f5905df0445003b720fc95ceb3b65b36f317
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "Allow setting the display of the TextLinkLayout element (#1262)"
    This reverts commit 3c064dc227e48c8e9429253510e530edd00e55e0.

--- src/BaseComponents/TextLinkLayout/TextLinkLayout.js
+++ src/BaseComponents/TextLinkLayout/TextLinkLayout.js
@@ -9,15 +9,13 @@ export default class TextLinkLayout extends WixComponent {
     children: PropTypes.node,
     underlineStyle: PropTypes.oneOf(['always', 'hover', 'never']),
     darkBackground: PropTypes.bool,
-    size: PropTypes.oneOf(['small', 'medium']),
-    display: PropTypes.oneOf(['block', 'inline-block'])
+    size: PropTypes.oneOf(['small', 'medium'])
   };
 
   static defaultProps = {
     underlineStyle: 'hover',
     darkBackground: false,
-    size: 'medium',
-    display: 'block'
+    size: 'medium'
   };
 
   constructor(props) {
@@ -37,13 +35,12 @@ export default class TextLinkLayout extends WixComponent {
 
   render() {
     const {isHover} = this.state;
-    const {darkBackground, underlineStyle, size, children, display} = this.props;
+    const {darkBackground, underlineStyle, size, children} = this.props;
 
     const color = darkBackground ? (isHover ? '#F0F4F7' : '#FFFFFF') : (isHover ? '#4EB7F5' : '#3899EC');
 
     const style = {
       color,
-      display,
       background: 'none',
       cursor: 'pointer',
       textDecoration: ((underlineStyle === 'hover' && isHover) || underlineStyle === 'always') ? 'underline' : 'none'
