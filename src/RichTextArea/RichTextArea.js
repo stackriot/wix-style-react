22e39388c5752a2062fc3c667f5f99125160375f
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "Rich text area bugs v2 (#1886)"
    This reverts commit ab33273313ea3ca026b3e4b0c255c4f7b23b5102.

--- src/RichTextArea/RichTextArea.js
+++ src/RichTextArea/RichTextArea.js
@@ -168,7 +168,6 @@ class RichTextArea extends WixComponent {
   hasLink = () => this.state.editorState.inlines.some(inline => inline.type === 'link');
 
   handleButtonClick = (action, type) => {
-    this.setState({activeToolbarButton: type})
     switch (action) {
       case 'mark':
         return this.handleMarkButtonClick(type);
@@ -321,10 +320,6 @@ class RichTextArea extends WixComponent {
       <div className={className} data-hook={dataHook}>
         <div className={classNames(styles.toolbar, {[styles.disabled]: disabled})} data-hook='toolbar'>
           <RichTextEditorToolbar
-            /*
-              activeToolbarButton prop required to trigger RichTextEditorToolbar re-render after toolbar button click
-            */
-            activeToolbarButton={this.state.activeToolbarButton}
             selection={editorState.fragment.text}
             disabled={disabled}
             onClick={this.handleButtonClick}
