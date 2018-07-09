316c1758867cfd013dfacae54941202c5175de3f
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "Check for existence of document after timeout in tooltip (#631)"
    This reverts commit 0ea597379c3e93ea260c0ea142c5c577ad3cdfae.

--- src/Tooltip/Tooltip.js
+++ src/Tooltip/Tooltip.js
@@ -219,9 +219,6 @@ class Tooltip extends WixComponent {
     }
     if (!this.state.visible) {
       this._showTimeout = setTimeout(() => {
-        if (typeof document === 'undefined') {
-          return;
-        }
         if (this.props.onShow) {
           this.props.onShow();
         }
