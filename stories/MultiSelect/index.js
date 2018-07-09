e9e95db042cd7d064dddcb6306994ee507a5011f
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "multiselect/expose tags testkit and add testkit doc file (#788)"
    This reverts commit 29676f1b51a2c0045185b1d19624aee56450bdc5.

--- stories/MultiSelect/index.js
+++ stories/MultiSelect/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import Markdown from '../utils/Components/Markdown';
-import TabbedView from '../utils/Components/TabbedView';
 import CodeExample from '../utils/Components/CodeExample';
 import Readme from '../../src/MultiSelect/README.md';
-import ReadmeTestKit from '../../src/MultiSelect/README.TESTKIT.md';
 
 import ExampleStandard from './ExampleStandard';
 import ExampleStandardRaw from '!raw-loader!./ExampleStandard';
@@ -13,18 +11,17 @@ import ExampleWithLimitedHeightRaw from '!raw-loader!./ExampleWithLimitedHeight'
 
 storiesOf('3. Inputs', module)
   .add('3.8 Tags', () => (
-    <TabbedView tabs={['API', 'TestKits']}>
-      <div>
-        <Markdown source={Readme}/>
-        <h1>Usage examples</h1>
-        <CodeExample title="Standard" code={ExampleStandardRaw}>
-          <ExampleStandard/>
-        </CodeExample>
+    <div>
+      <Markdown source={Readme}/>
 
-        <CodeExample title="Limited height" code={ExampleWithLimitedHeightRaw}>
-          <ExampleWithLimitedHeight/>
-        </CodeExample>
-      </div>
-      <Markdown source={ReadmeTestKit}/>
-    </TabbedView>
+      <h1>Usage examples</h1>
+
+      <CodeExample title="Standard" code={ExampleStandardRaw}>
+        <ExampleStandard/>
+      </CodeExample>
+
+      <CodeExample title="Limited height" code={ExampleWithLimitedHeightRaw}>
+        <ExampleWithLimitedHeight/>
+      </CodeExample>
+    </div>
   ));
