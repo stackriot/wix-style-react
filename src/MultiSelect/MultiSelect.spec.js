a83a0c9fba4c54018899caffb5f5f7bf4378d570
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "fixes MultiSelect test so it is not dependent on lodash uniqueId calls (#1318)"
    This reverts commit a94cd3f9ef6c5b235aec5b50434527ac0220d54c.

--- src/MultiSelect/MultiSelect.spec.js
+++ src/MultiSelect/MultiSelect.spec.js
@@ -192,11 +192,10 @@ describe('multiSelect', () => {
     inputDriver.trigger('paste');
     inputDriver.enterText(`${options[0].value}, Arkansa`);
     expect(onChange).toBeCalledWith({target: {value: ''}});
-    const onSelectCallArgs = onSelect.mock.calls[0][0];
-    expect(onSelectCallArgs[0]).toEqual({id: options[0].id, label: options[0].value});
-    expect(onSelectCallArgs[1].label).toEqual('Arkansa');
-    expect(onSelectCallArgs[1].theme).toEqual('error');
-    expect(onSelectCallArgs[1].id.startsWith('customOption_')).toEqual(true);
+    expect(onSelect).toBeCalledWith([
+      {id: options[0].id, label: options[0].value},
+      {id: 'customOption_1', label: 'Arkansa', theme: 'error'}
+    ]);
   });
 
   it('should call onManuallyInput after delimiter is pressed and input is not empty', () => {
