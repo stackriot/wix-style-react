7100f727566e5fa77ec9b2bb09f11a1f10a250b9
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "change SideMenu Header button to clickable div (#1900)"
    This reverts commit 5170c5825b9bc5adc2925b53de09435b78418007.

--- stories/SideMenu/ExampleStandard.js
+++ stories/SideMenu/ExampleStandard.js
@@ -2,68 +2,35 @@ import React from 'react';
 
 import {
   SideMenu,
-  Button,
-  Tooltip
+  Button
 } from 'wix-style-react';
 
 import {
-  PenOutline,
+  Help as HelpIcon,
   Chat as ChatIcon,
-  Preview
+  Trash3 as TrashIcon
 } from 'wix-style-react/Icons';
 
 export default () =>
   <div style={{width: 220, height: 700}}>
     <SideMenu>
-      <SideMenu.Header>
-        <div style={{padding: '26px 30px', fontSize: '20px', color: 'white'}}>
-          <Tooltip
-            content="wix-style-react"
-            placement="bottom"
-            alignment="left"
-            dataHook="site-name-tooltip"
-            maxWidth="250"
-            >
-            <div>
-              <span style={{marginRight: '5px', fontSize: '20px'}}>wix-style-react</span>
-              <Preview size={12}/>
-            </div>
-          </Tooltip>
-          <div style={{marginTop: '5px', fontSize: '13px'}}>Role: Owner</div>
-        </div>
-
+      <SideMenu.Header onClick={() => console.log('Header clicked')}>
+        <TrashIcon size="5em"/>
+        <h2 style={{color: '#fff'}}>My Application</h2>
       </SideMenu.Header>
 
       <SideMenu.Navigation>
-        <SideMenu.NavigationLink onClick={() => console.log('Dashboard clicked')}>
-          Dashboard
+        <SideMenu.NavigationLink onClick={() => console.log('#1 clicked')} href="//wix.com">
+          Link #1
         </SideMenu.NavigationLink>
-        <SideMenu.NavigationLink onClick={() => console.log('Rest. Menus clicked')}>
-          Rest. Menus
-        </SideMenu.NavigationLink>
-        <SideMenu.NavigationLink withArrow onClick={() => console.log('Rest. Products clicked')}>
-          Rest. Products
-        </SideMenu.NavigationLink>
-        <SideMenu.NavigationLink isActive onClick={() => console.log('Store Orders clicked')}>
-          Store Orders
-        </SideMenu.NavigationLink>
-        <SideMenu.NavigationLink withArrow onClick={() => console.log('Contacts & CRM clicked')}>
-          Contacts & CRM
-        </SideMenu.NavigationLink>
-        <SideMenu.NavigationLink withArrow onClick={() => console.log('Marketing Tools clicked')}>
-          Marketing Tools
+        <SideMenu.NavigationLink withArrow onClick={() => console.log('#2 clicked')}>
+          Link #2
         </SideMenu.NavigationLink>
 
         <SideMenu.NavigationSeparator/>
 
-        <SideMenu.NavigationLink onClick={() => console.log('Manage Website clicked')}>
-          Manage Website
-        </SideMenu.NavigationLink>
-        <SideMenu.NavigationLink badge={<SideMenu.NavigationBadge/>} onClick={() => console.log('Settings clicked')}>
-          Settings
-        </SideMenu.NavigationLink>
-        <SideMenu.NavigationLink onClick={() => console.log('Apps clicked')}>
-          Apps
+        <SideMenu.NavigationLink badge={<SideMenu.NavigationBadge/>} isActive onClick={() => console.log('#3 clicked')}>
+          Link #3
         </SideMenu.NavigationLink>
 
         <SideMenu.NavigationLink
@@ -80,19 +47,17 @@ export default () =>
           theme="fullpurple"
           onClick={() => console.log('Promotion button clicked!')}
           >
-          Upgrade
+          Buy 1 for price of 2!
         </Button>
       </SideMenu.Promotion>
-
-      <SideMenu.NavigationSeparator/>
 
       <SideMenu.Footer>
         <SideMenu.FooterLink
           href="https://support.wix.com/"
           target="_blank"
-          icon={<PenOutline size="1em"/>}
+          icon={<HelpIcon size="1em"/>}
           >
-          Edit
+          Help Me!
         </SideMenu.FooterLink>
 
         <SideMenu.FooterTinyLink
