e2c25f0d006cb8bdf65b25cdf9e7bcca3a620dcb
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "Fixed onCancel and onConfirm handlers. Add support of drag/click events for picking colors. (#839)"
    This reverts commit 3f5b1bf4a6e4bc4188a03f9f18217f2174380272.

--- src/ColorPicker/color-picker-actions.js
+++ src/ColorPicker/color-picker-actions.js
@@ -8,11 +8,11 @@ import css from './color-picker-actions.scss';
 
 const ColorPickerActions = ({onCancel, onConfirm}) =>
   <div className={css.root}>
-    <Button height="small" theme="icon-standardsecondary" onClick={onCancel}>
-      <Close size="12px"/>
+    <Button height="small" theme="icon-standardsecondary">
+      <Close size="12px" onClick={onCancel}/>
     </Button>
-    <Button height="small" theme="icon-standard" onClick={onConfirm}>
-      <V size="12px"/>
+    <Button height="small" theme="icon-standard">
+      <V size="12px" onClick={onConfirm}/>
     </Button>
   </div>;
 
