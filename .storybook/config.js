206b0359bdaec4d14a90bd594045c9d81d0407f3
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "add editable selector (#915)"
    This reverts commit 63cd949259c7660450d150968e914aadaf408439.

--- .storybook/config.js
+++ .storybook/config.js
@@ -84,7 +84,6 @@ function loadStories() {
 
   require('../stories/MessageBox');
   require('../stories/Modal');
-  require('../stories/EditableSelector');
   require('../stories/ModalSelector');
   require('../stories/Selector');
 }
