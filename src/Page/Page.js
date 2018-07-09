001029094b728c9c9a274690fc7a246c684f9b97
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "Style fixes for page (#1494)"
    This reverts commit 48d86e7bd96816ee5b9f71c4fe37e3c22a9cac7f.

--- src/Page/Page.js
+++ src/Page/Page.js
@@ -142,7 +142,6 @@ class Page extends WixComponent {
     this._setContainerScrollTopThreshold(PageTail && hasGradientClassName);
     const imageHeight = `${headerHeight + (PageTail ? -tailHeight : 39)}px`;
     const gradientHeight = gradientCoverTail ? `${headerHeight + (PageTail ? -SCROLL_TOP_THRESHOLD : 39)}px` : imageHeight;
-    const calculatedHeaderHeight = !minimized ? headerHeight : PageTail ? headerHeight - 78 : headerHeight - 54;
 
     return (
       <div className={s.page}>
@@ -181,7 +180,7 @@ class Page extends WixComponent {
           data-hook="page-scrollable-content"
           ref={r => this.scrollableContentRef = r}
           >
-          <div className={s.contentPlaceholder} style={{height: `${calculatedHeaderHeight}px`}}/>
+          <div className={s.contentPlaceholder} style={{height: `${headerHeight}px`}}/>
           {
             hasBackgroundImage &&
               <div
