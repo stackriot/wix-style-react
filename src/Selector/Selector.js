a1b9580aa8a58fadad48daceccd63f710054558e
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "<Selector/ >- remove the unused ExtraIcon component (#1736)"
    This reverts commit e16d553873fd4515d64640ca8d1e6ef56407e6a7.

--- src/Selector/Selector.js
+++ src/Selector/Selector.js
@@ -7,6 +7,7 @@ import RadioButton from '../RadioGroup/RadioButton/RadioButton';
 import Text from '../Text';
 import styles from './Selector.scss';
 import ExtraText from './ExtraText';
+import ExtraIcon from './ExtraIcon';
 import ProgressBar from './ProgressBar';
 
 class Selector extends WixComponent {
@@ -105,6 +106,7 @@ class Selector extends WixComponent {
 }
 
 Selector.ExtraText = ExtraText;
+Selector.ExtraIcon = ExtraIcon;
 Selector.ProgressBar = ProgressBar;
 
 export default Selector;
