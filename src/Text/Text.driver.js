351cc3ce45cc514cee986cfa89dd292ad6d7334d
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "Text ellipsis (#910)"
    This reverts commit 869b30de5fb6ff597e89918778a2c18c88f67523.

--- src/Text/Text.driver.js
+++ src/Text/Text.driver.js
@@ -1,8 +1,6 @@
 export default ({element}) => ({
   getType: () => element.tagName.toLowerCase(),
   getText: () => element.textContent,
-  hasEllipsis: () => element.className.includes('ellipsis'),
-  getTitle: () => element.title,
   getClassName: () => element.className
 });
 
