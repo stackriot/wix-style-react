6b6feddf28a62f7f9955b0a7315c88fba05e1e5b
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "improve Badge & CounterBadge stories (#1610)"
    This reverts commit 06ffb41ed318bb8748ba41f6929234b8e79790a5.

--- stories/CounterBadge/Example.js
+++ stories/CounterBadge/Example.js
@@ -3,7 +3,6 @@ import {Email} from 'wix-style-react/Icons';
 import DropdownComposite from '../../src/DropdownComposite';
 import Dropdown from '../../src/Dropdown';
 import Label from '../../src/Label';
-import Text from '../../src/Text';
 import {SKIN} from 'wix-ui-backoffice/dist/src/components/StylableCounterBadge/constants';
 import style from './CounterBadge.scss';
 import CounterBadge from 'wix-style-react/CounterBadge';
@@ -22,26 +21,23 @@ class ControlledCounterBadgeExample extends React.Component {
   }
   render() {
     return (
-      <div>
-        <div style={{background: 'azure', width: '420px'}}><Text>{`import CounterBadge from 'wix-style-react/CounterBadge';`}</Text></div>
-        <div className={style.container}>
-          <div style={{width: '200px'}}>
-            <h1> Props </h1>
-            <DropdownComposite><Label>children</Label><Dropdown selectedId={this.state.children} options={childrenOptions} onSelect={option => this.setState({children: option.value})}/></DropdownComposite>
-            <DropdownComposite><Label>skin</Label><Dropdown selectedId={this.state.skin} options={skinOptions} onSelect={option => this.setState({skin: option.value})}/></DropdownComposite>
-          </div>
-          <div>
-            <h1> Preview </h1>
+      <div className={style.container}>
+        <div style={{width: '200px'}}>
+          <h1> Props </h1>
+          <DropdownComposite><Label>children</Label><Dropdown selectedId={this.state.children} options={childrenOptions} onSelect={option => this.setState({children: option.value})}/></DropdownComposite>
+          <DropdownComposite><Label>skin</Label><Dropdown selectedId={this.state.skin} options={skinOptions} onSelect={option => this.setState({skin: option.value})}/></DropdownComposite>
+        </div>
+        <div>
+          <h1> Preview </h1>
 
-            <br/><br/><br/><br/><br/><br/>
+          <br/><br/><br/><br/><br/><br/>
 
-            <CounterBadge
-              skin={this.state.skin}
-              data-hook="storybook-counterbadge"
-              >
-              {this.state.children === 'Email' ? <Email/> : this.state.children}
-            </CounterBadge>
-          </div>
+          <CounterBadge
+            skin={this.state.skin}
+            data-hook="storybook-counterbadge"
+            >
+            {this.state.children === 'Email' ? <Email/> : this.state.children}
+          </CounterBadge>
         </div>
       </div>
     );
