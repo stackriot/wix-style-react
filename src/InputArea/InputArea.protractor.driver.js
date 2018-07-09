8438f9adf267d951526e416994176f030f01f0c4
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "InputArea+RichTextArea: Add new Focus styles (#1553)"
    This reverts commit a31d06b599c2a5dc72f267f2d2a3ad3c67306ae5.

--- src/InputArea/InputArea.protractor.driver.js
+++
@@ -1,9 +0,0 @@
-
-import {isFocused} from '../test-common';
-
-const toggleSwitchDriverFactory = component => ({
-  element: () => component,
-  isFocused: async () => isFocused(component.$('textarea'))
-});
-
-export default toggleSwitchDriverFactory;
