311b89f59b4ce6e521ee9bedcac36a08fb7aff3c
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "Migrate Checkbox story to new "story()" (#1035)"
    This reverts commit cc7c16d8c9d756bdf11ee6eb8f744ef069be5717.

--- stories/Checkbox/index.js
+++ stories/Checkbox/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import story from '../utils/Components/Story';
+import {storiesOf} from '@storybook/react';
+import Markdown from '../utils/Components/Markdown';
 import CodeExample from '../utils/Components/CodeExample';
+
+import CheckboxSource from '!raw-loader!../../src/Checkbox/Checkbox';
+import AutoDocs from '../utils/Components/AutoDocs';
+
+import TabbedView from '../utils/Components/TabbedView';
+import ReadmeTestKit from '../../src/Checkbox/README.TESTKIT.md';
 
 import ExampleStandard from './ExampleStandard';
 import ExampleStandardRaw from '!raw-loader!./ExampleStandard';
@@ -14,28 +21,25 @@ import ExampleRtlRaw from '!raw-loader!./ExampleRtl';
 import ExampleControlled from './ExampleControlled';
 import ExampleControlledRaw from '!raw-loader!./ExampleControlled';
 
-story({
-  category: 'Core',
-  componentSrcFolder: 'Checkbox',
-  componentProps: {
-    children: 'Some text'
-  },
-  examples: (
-    <div>
-      <h1>Usage examples</h1>
-      <CodeExample title="Standard" code={ExampleStandardRaw}>
-        <ExampleStandard/>
-      </CodeExample>
-      <CodeExample title="Indeterminate" code={ExampleIndeterminateRaw}>
-        <ExampleIndeterminate/>
-      </CodeExample>
-      <CodeExample title="Rtl" code={ExampleRtlRaw}>
-        <ExampleRtl/>
-      </CodeExample>
-      <CodeExample title="Controlled input" code={ExampleControlledRaw}>
-        <ExampleControlled/>
-      </CodeExample>
-    </div>
-  )
-});
-
+storiesOf('Core', module)
+  .add('Checkbox', () => (
+    <TabbedView tabs={['API', 'TestKits']}>
+      <div>
+        <AutoDocs source={CheckboxSource}/>
+        <h1>Usage examples</h1>
+        <CodeExample title="Standard" code={ExampleStandardRaw}>
+          <ExampleStandard/>
+        </CodeExample>
+        <CodeExample title="Indeterminate" code={ExampleIndeterminateRaw}>
+          <ExampleIndeterminate/>
+        </CodeExample>
+        <CodeExample title="Rtl" code={ExampleRtlRaw}>
+          <ExampleRtl/>
+        </CodeExample>
+        <CodeExample title="Controlled input" code={ExampleControlledRaw}>
+          <ExampleControlled/>
+        </CodeExample>
+      </div>
+      <Markdown source={ReadmeTestKit}/>
+    </TabbedView>
+  ));
