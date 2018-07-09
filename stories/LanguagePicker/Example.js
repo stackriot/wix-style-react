ba703012b9b5c6bb4f822070fc7335bcab8b4a76
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "added dividers to languagePicker (#595)"
    This reverts commit e4cec6b74cb8a1ec56f082c8a20a9ca033de9cad.

--- stories/LanguagePicker/Example.js
+++ stories/LanguagePicker/Example.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import {LanguagePicker} from 'wix-style-react';
 
-const mockDictionary = {
-  en: 'Hello',
-  fr: 'Bonjour',
-  tr: 'Merhaba'
-};
+const Example = () => (
+  <div style={{display: 'flex', alignItems: 'center', marginLeft: '30px'}}>
+    <LanguagePicker dataHook="story-languagePicker" onSelect={locale => console.log('locale: ', locale)}>
+      <LanguagePicker.Option languageKey="en">English</LanguagePicker.Option>
+      <LanguagePicker.Option languageKey="fr">French</LanguagePicker.Option>
+      <LanguagePicker.Option languageKey="tr">Turkish</LanguagePicker.Option>
+    </LanguagePicker>
+  </div>
+);
 
-export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {languageKey: 'en'}
-  }
-  render() {
-    return (
-      <div style={{display: 'flex', alignItems: 'center', marginLeft: '30px'}}>
-        <LanguagePicker dataHook="story-languagePicker" onSelect={languageKey => this.setState({languageKey})}>
-          <LanguagePicker.Option languageKey="en">English</LanguagePicker.Option>
-          <LanguagePicker.Option languageKey="fr">French</LanguagePicker.Option>
-          <LanguagePicker.Option languageKey="tr">Turkish</LanguagePicker.Option>
-        </LanguagePicker>
-        <h1>{mockDictionary[this.state.languageKey]}</h1>
-      </div>
-    );
-  }
-}
+
+export default Example;
