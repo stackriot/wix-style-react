c1b665ce864fd855e9c48deb4f7711c3660f8781
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "Chaged version (#1295)"
    This reverts commit dccd803e1a425635970531059ca70d98b0140bf7.

--- stories/Page/index.js
+++ stories/Page/index.js
@@ -6,7 +6,7 @@ import Button from '../../src/Backoffice/Button';
 import SomeContentComponent from './SomeContentComponent';
 import Breadcrumbs from './Breadcrumbs';
 import SomeTailComponent from './SomeTailComponent';
-import './Page.scss';
+import s from './Page.scss';
 
 const header = (
   <Page.Header
@@ -20,9 +20,9 @@ const header = (
     />
 );
 
-const content = showScss => (
+const content = (
   <Page.Content>
-    <SomeContentComponent showScss={showScss}/>
+    <SomeContentComponent/>
   </Page.Content>
 );
 
@@ -40,9 +40,9 @@ story({
   name: 'Page',
   componentSrcFolder: 'Page',
   componentProps: {
-    children: [header, tail, content(false)],
+    children: [header, tail, content],
     dataHook: 'story-page',
-    gradientClassName: 'background-gradient',
+    gradientClassName: s.backgroundGradient,
     backgroundImageUrl: 'https://static.wixstatic.com/media/a9ff3b_9928686dcfa740bd802821d0b6f4ac03.jpg/v1/fill/w_1000,h_250,al_c,q_85,usm_0.66_1.00_0.01/a9ff3b_9928686dcfa740bd802821d0b6f4ac03.jpg'
   },
   exampleProps: {
@@ -50,16 +50,16 @@ story({
       '',
       'https://static.wixstatic.com/media/a9ff3b_9928686dcfa740bd802821d0b6f4ac03.jpg/v1/fill/w_1000,h_250,al_c,q_85,usm_0.66_1.00_0.01/a9ff3b_9928686dcfa740bd802821d0b6f4ac03.jpg'
     ],
-    gradientClassName: ['', 'background-gradient']
+    gradientClassName: ['', s.backgroundGradient]
   }
 });
 
 storiesOf(category, module)
   .add('10.2 Page example', () => (
-    <div data-hook="story-page-example">
-      <div data-hook="side-bar">Sidebar</div>
-      <div data-hook="body-content">
-        <div data-hook="top-bar">TopBar</div>
-        <Page>{[header, tail, content(true)]}</Page>
+    <div>
+      <div className={s.siderBar}>Sidebar</div>
+      <div className={s.bodyContent}>
+        <div className={s.topBar}>TopBar</div>
+        <Page>{[header, tail, content]}</Page>
       </div>
     </div>));
