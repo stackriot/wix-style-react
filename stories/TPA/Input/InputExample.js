85d5eb160a5b22e599288bfda33e4a7ffcef86fb
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "Tpa input testkit (#739)"
    This reverts commit 7c58234957800cb520eb3945c09949595f42b436.

--- stories/TPA/Input/InputExample.js
+++ stories/TPA/Input/InputExample.js
@@ -2,18 +2,13 @@ import React, {Component} from 'react';
 import Input from '../../../src/TPA/Input';
 import styles from './styles.scss';
 
-const InputExample = () => {
-  return (
-    <div style={{width: '300px'}}>
-      <Input dataHook="story-input" placeholder="Default place holder" type="number"/>
-    </div>
-  );
-}
+const style = {};
+
 
 export const InputValidationErrorExample = () => {
   return (
     <div style={{width: '300px'}}>
-      <Input dataHook="story-input-error" error placeholder="Input in error state"/>
+      <Input error placeholder="Input in error state"/>
     </div>
   );
 };
@@ -21,7 +16,7 @@ export const InputValidationErrorExample = () => {
 export const InputWithCustomStyleExample = () => {
   return (
     <div style={{width: '300px'}}>
-      <Input dataHook="story-input-style" inputClassName={styles.customInput} placeholder="Input with custom style"/>
+      <Input inputClassName={styles.customInput} placeholder="Input with custom style"/>
     </div>
   );
 };
@@ -29,10 +24,18 @@ export const InputWithCustomStyleExample = () => {
 export const InputWithCustomErrorExample = () => {
   return (
     <div style={{width: '300px'}}>
-      <Input dataHook="story-input-error-style" error errorClassName={styles.customError} placeholder="Input with custom error style"/>
+      <Input error errorClassName={styles.customError} placeholder="Input with custom error style"/>
     </div>
   );
 };
+
+const InputExample = () => {
+  return (
+    <div style={{width: '300px'}}>
+      <Input placeholder="Default place holder" type="number"/>
+    </div>
+  );
+}
 
 export default InputExample;
 
