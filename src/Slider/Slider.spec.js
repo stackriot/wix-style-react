cd83e3ce548a51fd074f5e781e42f2cfcf022594
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "Slider Testkit and e2e (#1303)"
    This reverts commit a8fa8c01145006db2e004521bf8e22096b9a5cb0.

--- src/Slider/Slider.spec.js
+++ src/Slider/Slider.spec.js
@@ -1,62 +1,63 @@
 import React from 'react';
+import _ from 'lodash/fp';
+import {mount} from 'enzyme';
 import Slider from './Slider';
-import {createDriverFactory} from '../test-common';
-import sliderDriverFactory from './Slider.driver';
-import {sliderTestkitFactory} from '../../testkit';
+import {componentFactory, sliderDriverFactory} from './testkit/Slider';
 import {sliderTestkitFactory as enzymeSliderTestkitFactory} from '../../testkit/enzyme';
-import {isTestkitExists, isEnzymeTestkitExists} from '../../testkit/test-common';
 
 describe('Slider', () => {
-
-  const createDriver = createDriverFactory(sliderDriverFactory);
   let driver;
+
+  const createDriver = _.compose(sliderDriverFactory, componentFactory);
 
   it('should render slider', () => {
     const onChange = jest.fn(value => this.setState({value}));
-    const props = {value: [3], min: 1, max: 10, onChange};
+    const selectedValue = 3;
+    const min = 1;
+    const max = 10;
 
-    driver = createDriver(<Slider {...props}/>);
+    driver = createDriver({onChange, value: [selectedValue], min, max});
 
     expect(driver.numOfSliderDots()).toBe(10);
-    expect(driver.numOfSliderHandles()).toBe(1);
-    expect(driver.isDotSelected(3)).toBe(true);
+    expect(driver.numOfSLiderHandles()).toBe(1);
+    expect(driver.isDotSelected(selectedValue)).toBe(true);
   });
 
   it('should render slider with multi-range', () => {
     const onChange = jest.fn(value => this.setState({value}));
-    const props = {value: [3, 5, 7], min: 1, max: 10, onChange};
+    const selectedValues = [3, 5, 7];
+    const min = 1;
+    const max = 10;
 
-    driver = createDriver(<Slider {...props}/>);
+    driver = createDriver({onChange, value: selectedValues, min, max});
 
     expect(driver.numOfSliderDots()).toBe(10);
-    expect(driver.numOfSliderHandles()).toBe(3);
-
-    props.value.forEach(selectedValue => {
+    expect(driver.numOfSLiderHandles()).toBe(3);
+    selectedValues.forEach(selectedValue => {
       expect(driver.isDotSelected(selectedValue)).toBe(true);
     });
   });
 
   it('should show correct value on hover', () => {
     const onChange = jest.fn(value => this.setState({value}));
-    const props = {value: [3, 5, 7], min: 1, max: 10, onChange};
+    const selectedValues = [3, 5, 7];
+    const min = 1;
+    const max = 10;
 
-    driver = createDriver(<Slider {...props}/>);
+    driver = createDriver({onChange, value: selectedValues, min, max});
 
     driver.hoverHandle({handleIndex: 0});
 
-    expect(driver.getToolTipValue()).toBe(`${props.value[0]}`);
+    expect(driver.getToolTipValue()).toBe(`${selectedValues[0]}`);
 
     driver.unHoverHandle({handleIndex: 0});
     driver.hoverHandle({handleIndex: 1});
 
-    expect(driver.getToolTipValue()).toBe(`${props.value[1]}`);
+    expect(driver.getToolTipValue()).toBe(`${selectedValues[1]}`);
   });
 
   it('should not display tooltip if `displayTooltip` is set to false', () => {
-    const onChange = jest.fn(value => this.setState({value}));
-    const props = {value: [4], displayTooltip: false, onChange};
-
-    driver = createDriver(<Slider {...props}/>);
+    driver = createDriver({value: [4], displayTooltip: false});
 
     driver.hoverHandle({handleIndex: 0});
 
@@ -66,14 +67,15 @@ describe('Slider', () => {
   });
 });
 
-describe('testkit', () => {
-  it('should exist', () => {
-    expect(isTestkitExists(<Slider onChange={() => {}}/>, sliderTestkitFactory)).toBe(true);
-  });
-});
 
 describe('enzyme testkit', () => {
   it('should exist', () => {
-    expect(isEnzymeTestkitExists(<Slider onChange={() => {}}/>, enzymeSliderTestkitFactory)).toBe(true);
+    const onChange = jest.fn(value => this.setState({value}));
+    const value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const dataHook = 'slider-enzyme';
+    const wrapper = mount(<div><Slider onChange={onChange} data-hook={dataHook} value={value} min={1} max={10} step={1}/></div>);
+    const sliderTestkit = enzymeSliderTestkitFactory({wrapper, dataHook});
+
+    expect(sliderTestkit.exists()).toBe(true);
   });
 });
