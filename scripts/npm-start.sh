55e4fe9398aaa4c4d18f6f12cc944e0de525a8dd
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "Chore/npm scripts (#1157)"
    This reverts commit 4ee1e5176e8a5270a98adfda53cf45652029ee97.

--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-if scripts/ensure-dist.sh; then
-  npm run storybook & yoshi start
+if ! test -d dist; then
+  echo "Welcome to Wix Style React!"
+  echo ""
+  echo "allow me to build project and prepare storybook for first run..."
+  echo ""
+
+  npm run build
 fi
+
+svg2react-icon-once
+npm run storybook & yoshi start
