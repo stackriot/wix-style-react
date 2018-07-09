00abfc009342f4e9ac0d622e37492d07da8385bd
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "Notification badge (#813)"
    This reverts commit 04eaf616beab38f9b6024151864eb0daebc041ae.

--- stories/Badge/DefaultBadge.js
+++ stories/Badge/DefaultBadge.js
@@ -9,10 +9,6 @@ export default () => (
     <Badge type="success">Success</Badge>&nbsp;
     <Badge type="info">Info</Badge>&nbsp;
     <Badge type="warning">Warning</Badge>&nbsp;
-    <Badge type="danger">Danger</Badge>&nbsp;
-    <Badge type="businessManagerCounter">1</Badge>&nbsp;
-    <Badge type="businessManagerCounter">3</Badge>&nbsp;
-    <Badge type="businessManagerCounter">44</Badge>&nbsp;
-    <Badge type="businessManagerCounter">666</Badge>&nbsp;
+    <Badge type="danger">Danger</Badge>
   </div>
 );
