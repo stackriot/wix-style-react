ce917cf88fc78b58efe6ff77903066361aa03631
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "feat: add getInfoTooltipTestKit to TextField (#826)"
    This reverts commit a95126a6f092b489824de063a9311bb2f47d19f2.

--- src/FieldLabelAttributes/FieldLabelAttributes.driver.js
+++ src/FieldLabelAttributes/FieldLabelAttributes.driver.js
@@ -1,10 +1,8 @@
 import $ from 'jquery';
-import tooltipDriverFactory from '../../src/Tooltip/Tooltip.driver';
 
-const fieldLabelAttributesDriverFactory = ({element, wrapper}) => {
+const fieldLabelAttributesDriverFactory = ({element}) => {
   return {
     exists: () => !!element,
-    getTooltipTestKit: () => tooltipDriverFactory({wrapper, element: $(element).find('[data-hook="info"]')}),
     hasRequired: () => !!$(element).find('[data-hook="required"]').length,
     hasInfo: () => !!$(element).find('[data-hook="info"]').length
   };
