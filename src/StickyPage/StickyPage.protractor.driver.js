26fee8e977902b77e97af7b61ec1df62cefdfc7b
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "added e2e (#1135)"
    This reverts commit 09bf2ee4da0556d0a906d4350ae20d0e18ede214.

--- src/StickyPage/StickyPage.protractor.driver.js
+++ src/StickyPage/StickyPage.protractor.driver.js
@@ -2,10 +2,7 @@ import s from './StickyPage.scss';
 
 const stickyPageDriverFactory = component => ({
   element: () => component,
-  scrollDown: async () => {
-    const element = await component.$(`.${s.scrollableContent}`);
-    await browser.executeScript('arguments[0].scrollTop = 200;', await element.getWebElement());
-  },
+  scrollDown: () => browser.executeScript(`document.querySelectorAll('.${s.scrollableContent}').forEach(x => x.scrollTo(0,200));`),
   isHeaderMinimized: () => component.$$(`.minimized`).count().then(count => count > 0)
 });
 
