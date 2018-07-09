2a0c8c9bc8ef96f265b9d265eb7fea87948b1829
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "feat(Amaterial input UX fixes #1421) : added hover and fade in / out … (#1445)"
    This reverts commit 6a2e33584211a59ca3b3c724f14cc1fb4aed9382.

--- src/Input/ThemedInput.js
+++ src/Input/ThemedInput.js
@@ -37,7 +37,10 @@ class ThemedInput extends Input {
       [noLeftBorderRadius]: noLeftBorderRadius
     };
 
-    const placeholder = this.props.placeholder;
+    let placeholder = this.props.placeholder;
+    if (theme === 'amaterial' && !classes[styles.hasFocus] && !classes[styles.hasValue]) {
+      placeholder = '';
+    }
     return (
       <div
         className={classNames(classes, styles.root, styles[`theme-${theme}`], styles[`size-${size}${withSelection ? '-with-selection' : ''}`])}
