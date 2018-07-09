a35cb13339188883a4aa694797778d6224fbf9fe
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "tooltip location => set from props (#1552)"
    This reverts commit 16f58f7dbddb79de75682584ffdde7da52bb07ba.

--- src/Input/InputErrorSuffix.js
+++ src/Input/InputErrorSuffix.js
@@ -11,7 +11,7 @@ class InputErrorSuffix extends React.Component {
       <Tooltip
         dataHook="input-tooltip"
         disabled={this.props.errorMessage.length === 0}
-        placement={this.props.tooltipPlacement}
+        placement="top"
         alignment="center"
         textAlign="left"
         content={this.props.errorMessage}
@@ -30,8 +30,7 @@ class InputErrorSuffix extends React.Component {
 InputErrorSuffix.propTypes = {
   theme: PropTypes.oneOf(['normal', 'paneltitle', 'material', 'amaterial']),
   errorMessage: PropTypes.string.isRequired,
-  focused: PropTypes.bool,
-  tooltipPlacement: PropTypes.string
+  focused: PropTypes.bool
 };
 
 export default InputErrorSuffix;
