057ec91cc3431f50fb8359cb237ec872a060a98e
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "chore(release.js): better log messages (#1103)"
    This reverts commit 9718f9ed71b552d53e2c0d63b7d49752b9fe928b.

--- scripts/release.js
+++ scripts/release.js
@@ -7,33 +7,26 @@ if (!process.env.IS_BUILD_AGENT) {
 
 const path = require('path');
 const semver = require('semver');
-const exec = promisify(require('child_process').exec);
-const writeFile = promisify(require('fs').writeFile);
+const {exec} = require('child_process');
+const fs = require('fs');
 
 const packageJSONPath = path.resolve(__dirname, '..', 'package.json');
 const packageJSON = require(packageJSONPath);
 
-writeFile(
+promisify(fs.writeFile)(
   packageJSONPath,
   JSON.stringify(Object.assign({}, packageJSON, {private: true}), null, 2)
 )
 
-  .then(() => exec('npm view wix-style-react version --registry=https://registry.npmjs.org/'))
+  .then(() => promisify(exec)('npm view wix-style-react version --registry=https://registry.npmjs.org/'))
 
-  .then(publishedVersion =>
-    [
-      semver.gt(packageJSON.version, publishedVersion),
-      packageJSON.version,
-      publishedVersion
-    ]
-  )
+  .then(publishedVersion => semver.gt(packageJSON.version, publishedVersion))
 
-  .then(([shouldPublish, packageJSONVersion, publishedVersion]) =>
-    shouldPublish ?
-      writeFile(packageJSONPath, JSON.stringify(packageJSON, null, 2)).then(() =>
-        `Package will be published because version set in package.json ${packageJSONVersion} is newer than published ${publishedVersion}`) :
+  .then(shouldPublish => shouldPublish ?
+    promisify(fs.writeFile)(packageJSONPath, packageJSON)
+      .then(() => 'Package will be published because of a version bump in package.json') :
 
-      `Package will not be published because version ${packageJSONVersion} set in package.json is already published`
+    'Package will not be published because this version is already published'
   )
 
   .then(console.log)
@@ -45,7 +38,7 @@ writeFile(
 
 
 
-// promisify : Function -> List arguments -> Promise
+// promisify : Function -> List a -> Promise
 function promisify(fn) {
   return (...args) =>
     new Promise((resolve, reject) =>
