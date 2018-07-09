6434d428b6aada5747d8e754e2a5538396b79694
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "fix tooltip bug (#1137)"
    This reverts commit 63b51db57ac13f26c44caf0db081e766d8bd77e9.

--- src/Tooltip/Tooltip.js
+++ src/Tooltip/Tooltip.js
@@ -279,15 +279,21 @@ class Tooltip extends WixComponent {
           }
           this._showTimeout = null;
 
+          this.componentDidUpdate();
           let fw = 0;
           let sw = 0;
-          do {
-            this.componentDidUpdate();
-            const tooltipNode = ReactDOM.findDOMNode(this.tooltipContent);
-            fw = this._getRect(tooltipNode).width;
-            this._updatePosition(this.tooltipContent);
-            sw = this._getRect(tooltipNode).width;
-          } while (!this.props.appendToParent && fw !== sw);
+
+          this._showInterval = setInterval(() => {
+            if (this.tooltipContent !== undefined && this.tooltipContent !== null) {
+              clearInterval(this._showInterval);
+              do {
+                const tooltipNode = ReactDOM.findDOMNode(this.tooltipContent);
+                fw = this._getRect(tooltipNode).width;
+                this._updatePosition(this.tooltipContent);
+                sw = this._getRect(tooltipNode).width;
+              } while (!this.props.appendToParent && fw !== sw);
+            }
+          }, 1);
         });
       }, this.props.showDelay);
     }
