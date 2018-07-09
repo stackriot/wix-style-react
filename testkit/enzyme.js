c26d1b889a08354f3d7d1cbc3d207cf346b76fd4
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "refactor enzyme testing"
    This reverts commit c28f5325c67a99dc5ca84dd6339292c98bb88fd5.

--- testkit/enzyme.js
+++ testkit/enzyme.js
@@ -1,4 +1,4 @@
-import {enzymeTestkitFactoryCreator} from '../src/test-common';
+import {enzymeTestkitFactoryCreator, nativeEnzymeTestkitFactoryCreator} from '../src/test-common';
 
 import buttonSelectionDriverFactory from '../src/ButtonSelection/ButtonSelection.driver';
 export const buttonSelectionTestkitFactory = enzymeTestkitFactoryCreator(buttonSelectionDriverFactory);
@@ -145,13 +145,13 @@ import sideMenuDrillDriverFactory from '../src/SideMenu/DrillView/DrillView.driv
 export const sideMenuDrillTestkitFactory = enzymeTestkitFactoryCreator(sideMenuDrillDriverFactory);
 
 import tpaButtonDriverFactory from '../src/TPA/Button/Button.driver';
-export const tpaButtonTestkitFactory = enzymeTestkitFactoryCreator(tpaButtonDriverFactory);
+export const tpaButtonTestkitFactory = nativeEnzymeTestkitFactoryCreator(tpaButtonDriverFactory);
 
 import tpaInputDriverFactory from '../src/TPA/Input/Input.driver';
 export const tpaInputTestkitFactory = nativeEnzymeTestkitFactoryCreator(tpaInputDriverFactory);
 
 import tpaTextLinkDriverFactory from '../src/TPA/TextLink/TextLink.driver';
-export const tpaTextLinkTestkitFactory = enzymeTestkitFactoryCreator(tpaTextLinkDriverFactory);
+export const tpaTextLinkTestkitFactory = nativeEnzymeTestkitFactoryCreator(tpaTextLinkDriverFactory);
 
 import tpaFloatingTabsDriverFactory from '../src/TPA/FloatingTabs/FloatingTabs.driver';
 export const tpaFloatingTabsTestkitFactory = enzymeTestkitFactoryCreator(tpaFloatingTabsDriverFactory);
