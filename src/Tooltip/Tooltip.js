28bcfd954bf64bc5b508b64fca09f1faab8c64a7
Seth Bergman <sethbergman@users.noreply.github.com>
2018-07-09 05:21:36 -0500

    Revert "feat(tooltip): adding ability to select the element the tooltip is added to (#750)"
    This reverts commit 3198ac2cf44973dd1dbd90b4bce5d1433abe7f9e.

--- src/Tooltip/Tooltip.js
+++ src/Tooltip/Tooltip.js
@@ -56,7 +56,6 @@ class Tooltip extends WixComponent {
      * Just make sure the CSS are not leaked.
      */
     appendToParent: PropTypes.bool,
-    appendTo: PropTypes.any,
 
     /**
      * Allows to shift the tooltip position by x and y pixels.
@@ -209,9 +208,7 @@ class Tooltip extends WixComponent {
     if (typeof document === 'undefined') {
       return null;
     }
-    if (this.props.appendTo) {
-      return this.props.appendTo;
-    }
+
     return this.props.appendToParent ? this._childNode.parentElement : document ? document.body : null;
   }
 
@@ -370,7 +367,7 @@ class Tooltip extends WixComponent {
   }
 
   _getRect(el) {
-    if (this.props.appendTo || this.props.appendToParent) {
+    if (this.props.appendToParent) {
       // TODO: Once thoroughly tested, we could use the same approach in both cases.
       return {
         left: el.offsetLeft,
