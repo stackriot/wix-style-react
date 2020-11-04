34a3edb93fa60408954189cbb8afdf7204f55a67
Dima Feldman <dimafeldman@gmail.com>
2020-11-05 00:51:05 +0200

    Themes | Business Dashboard - fix CircularProgressBar styling (#6179)

--- src/Themes/businessDashboard/docs/components/CircularProgressBar.js
+++ src/Themes/businessDashboard/docs/components/CircularProgressBar.js
@@ -2,13 +2,8 @@ export const playground = `
 <ThemeProvider theme={theme()}>
   <Layout cols={2} gap="10px" justifyItems="center">
     <Cell>
-    <CircularProgressBar showProgressIndication size="small" value={50} />
-    </Cell>
-    <Cell>
-      <CircularProgressBar showProgressIndication value={50} />
-    </Cell>
-    <Cell>
-      <CircularProgressBar showProgressIndication size="large" value={50} />
+      <CircularProgressBar showProgressIndication value={100} />
+      <CircularProgressBar showProgressIndication value={30} />
     </Cell>
   </Layout>
 </ThemeProvider>
