c58d2e00b74bccd7a12df55712196dc0003deea7
Dima Feldman <dimafeldman@gmail.com>
2020-11-04 10:53:54 +0200

    Themes | Business Dashboard - improve Text & TextButton (#6173)

--- src/Themes/businessDashboard/docs/components/Text.js
+++ src/Themes/businessDashboard/docs/components/Text.js
@@ -2,29 +2,85 @@ export const playground = `
 <div>
   <ThemeProvider theme={theme()}> 
     <Box padding="3px">
-      <Text>Some text</Text>
-    </Box>
-    <Box backgroundColor="D10" padding="3px">
-      <Text light>Some light text</Text>
+      <Text>skin="standard" size="medium"</Text>
     </Box>
     <Box padding="3px">
-      <Text size="medium">Some medium text</Text>
-    </Box>
-    <Box backgroundColor="D10" padding="3px">
-      <Text size="medium" light>Some medium light text</Text>
+      <Text size="small">skin="standard" size="small"</Text>
     </Box>
     <Box padding="3px">
-      <Text size="small">Some small text</Text>
+      <Text size="tiny">skin="standard" size="tiny"</Text>
     </Box>
-    <Box backgroundColor="D10" padding="3px">
-      <Text size="small" light>Some small light text</Text>
+    <hr />
+    <Box padding="3px">
+      <Text skin="primary">skin="primary" size="medium"</Text>
     </Box>
     <Box padding="3px">
-      <Text size="tiny">Some tiny text</Text>
+      <Text skin="primary" size="small">skin="primary" size="small"</Text>
+    </Box>
+    <Box padding="3px">
+      <Text skin="primary" size="tiny">skin="primary" size="tiny"</Text>
+    </Box>
+    <hr />
+    <Box padding="3px">
+      <Text skin="disabled">skin="disabled" size="medium"</Text>
+    </Box>
+    <Box padding="3px">
+      <Text size="small" skin="disabled">skin="disabled" size="small"</Text>
+    </Box>
+    <Box padding="3px">
+      <Text size="tiny" skin="disabled">skin="disabled" size="tiny"</Text>
+    </Box>
+    <hr />
+    <Box padding="3px">
+      <Text weight="bold">skin="standard" size="medium" weight="bold"</Text>
+    </Box>
+    <Box padding="3px">
+      <Text size="small" weight="bold">skin="standard" size="small" weight="bold"</Text>
+    </Box>
+    <Box padding="3px">
+      <Text size="tiny" weight="bold">skin="standard" size="tiny" weight="bold"</Text>
+    </Box>
+    <hr />
+    <Box padding="3px">
+      <Text skin="primary" weight="bold">skin="primary" size="medium" weight="bold"</Text>
+    </Box>
+    <Box padding="3px">
+      <Text skin="primary" size="small" weight="bold">skin="primary" size="small" weight="bold"</Text>
+    </Box>
+    <Box padding="3px">
+      <Text skin="primary" size="tiny" weight="bold">skin="primary" size="tiny" weight="bold"</Text>
+    </Box>
+    <hr />
+    <Box padding="3px">
+      <Text secondary>skin="standard" size="medium" secondary</Text>
+    </Box>
+    <Box padding="3px">
+      <Text secondary size="small">skin="standard" size="small" secondary</Text>
+    </Box>
+    <Box padding="3px">
+      <Text secondary size="tiny">skin="standard" size="tiny" secondary</Text>
+    </Box>
+    <hr />
+    <Box backgroundColor="D10" padding="3px">
+      <Text light>skin="standard" size="medium" light</Text>
     </Box>
     <Box backgroundColor="D10" padding="3px">
-      <Text size="tiny" light>Some tiny light text</Text>
+      <Text size="small" light>skin="standard" size="small" light</Text>
     </Box>
+    <Box backgroundColor="D10" padding="3px">
+      <Text size="tiny" light>skin="standard" size="tiny" light</Text>
+    </Box>
+    <hr />
+    <Box backgroundColor="D10" padding="3px">
+      <Text light weight="bold">skin="standard" weight="bold" size="medium" light</Text>
+    </Box>
+    <Box backgroundColor="D10" padding="3px">
+      <Text size="small" light weight="bold">skin="standard" weight="bold" size="small" light</Text>
+    </Box>
+    <Box backgroundColor="D10" padding="3px">
+      <Text size="tiny" light weight="bold">skin="standard" weight="bold" size="tiny" light</Text>
+    </Box>
+    <hr />
   </ThemeProvider>
 </div>
 `;
--- src/Themes/businessDashboard/docs/components/TextButton.js
+++ src/Themes/businessDashboard/docs/components/TextButton.js
@@ -2,10 +2,54 @@ export const playground = `
 <div>
   <ThemeProvider theme={theme()}> 
     <Box padding="3px">
-      <TextButton>Default button</TextButton>
+      <TextButton size="tiny">Standard thin</TextButton>
     </Box>
     <Box padding="3px">
-      <TextButton weight="normal">Normal weight button</TextButton>
+      <TextButton size="tiny" weight="normal">Standard normal</TextButton>
+    </Box>
+    <Box padding="3px">
+      <TextButton size="tiny" disabled>Standard disabled</TextButton>
+    </Box>
+    <Box backgroundColor="D10" padding="3px">
+      <TextButton size="tiny" skin="light">Light</TextButton>
+    </Box>
+    <Box backgroundColor="D10" padding="3px">
+      <TextButton size="tiny" skin="light" disabled>Light disabled</TextButton>
+    </Box>
+    <Box padding="3px">
+      <TextButton size="tiny" skin="dark">Dark</TextButton>
+    </Box>
+    <Box padding="3px">
+      <TextButton size="tiny" skin="dark" disabled>Dark disabled</TextButton>
+    </Box>
+    <Box padding="3px">
+      <TextButton size="tiny">Tiny</TextButton>
+    </Box>
+    <Box padding="3px">
+      <TextButton size="small">Small</TextButton>
+    </Box>
+    <Box padding="3px">
+      <TextButton size="tiny" prefixIcon={<Icons.ChevronDownSmall />}>
+        Tiny with prefix
+      </TextButton>
+    </Box>
+    <Box padding="3px">
+      <TextButton size="tiny" suffixIcon={<Icons.ChevronDown />}>
+        Tiny with suffix
+      </TextButton>
+    </Box>
+    <Box padding="3px">
+      <TextButton size="small" prefixIcon={<Icons.ChevronDownSmall />}>
+        Small with prefix
+      </TextButton>
+    </Box>
+    <Box padding="3px">
+      <TextButton size="small" suffixIcon={<Icons.ChevronDown />}>
+        Small with suffix
+      </TextButton>
+    </Box>
+    <Box padding="3px">
+      <TextButton size="tiny" underline="always">With underline</TextButton>
     </Box>
   </ThemeProvider>
 </div>
