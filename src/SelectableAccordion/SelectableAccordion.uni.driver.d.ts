eaacfd151c32cb79df817ad70eebd476400e5561
Shai Ben Or <shaibo@wix.com>
2020-11-04 18:59:57 +0200

    <SelectableAccordion/> - Fix testkit types (#6181)
    * Add function params to driver interface
    
    * Add index to driver functions

--- src/SelectableAccordion/SelectableAccordion.uni.driver.d.ts
+++ src/SelectableAccordion/SelectableAccordion.uni.driver.d.ts
@@ -1,6 +1,6 @@
 import { BaseUniDriver } from 'wix-ui-test-utils/unidriver';
 
 export interface SelectableAccordionUniDriver extends BaseUniDriver {
-  clickItemAt(): Promise<void>;
-  isItemExpandedAt(): Promise<boolean>;
+  clickItemAt(idx: number): Promise<void>;
+  isItemExpandedAt(idx: number): Promise<boolean>;
 }
