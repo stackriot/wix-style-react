ed471106baa2bcbe86ae9ca372a59c5b4c5db0ae
Dima Feldman <dimafeldman@gmail.com>
2020-11-10 10:27:07 +0200

    Themes | Business Dashboard - adjust Button theme (#6203)

--- src/Themes/businessDashboard/docs/components/Button.js
+++ src/Themes/businessDashboard/docs/components/Button.js
@@ -2,8 +2,76 @@ export const playground = `
 <div>
 <ThemeProvider theme={theme()}>
   <Layout cols={4} gap="10px" alignItems="center">
-    <Button size="small">Small Button</Button>
-    <Button priority="secondary">Secondary</Button>
+    <Box padding="3px" backgroundColor="D80">
+      <Button size="small">Small</Button>
+    </Box>
+    <Box padding="3px" backgroundColor="D80">
+      <Button size="medium">Medium</Button>
+    </Box>
+    <Box padding="3px" backgroundColor="D80">
+      <Button priority="secondary" size="small">Secondary</Button>
+    </Box>
+    <Box padding="3px" backgroundColor="D80">
+      <Button priority="secondary">Secondary</Button>
+    </Box>
+  </Layout>
+  <Layout cols={4} gap="10px" alignItems="center">
+    <Box padding="3px" backgroundColor="D80">
+      <Button size="small" disabled>Small</Button>
+    </Box>
+    <Box padding="3px" backgroundColor="D80">
+      <Button size="medium" disabled>Medium</Button>
+    </Box>
+    <Box padding="3px" backgroundColor="D80">
+      <Button priority="secondary" size="small" disabled>Secondary</Button>
+    </Box>
+    <Box padding="3px" backgroundColor="D80">
+      <Button priority="secondary" disabled>Secondary</Button>
+    </Box>
+  </Layout>
+  <Layout cols={4} gap="10px" alignItems="center">
+    <Box padding="3px" backgroundColor="D80">
+      <Button size="small" prefixIcon={<Icons.ChevronDownSmall />}>
+        Prefix
+      </Button>
+    </Box>  
+    <Box padding="3px" backgroundColor="D80">
+      <Button priority="secondary" size="small" prefixIcon={<Icons.ChevronDownSmall />}>
+        Prefix
+      </Button>
+    </Box>  
+    <Box padding="3px" backgroundColor="D80">
+      <Button size="small" suffixIcon={<Icons.ChevronDownSmall />}>
+        Suffix
+      </Button>
+    </Box>  
+    <Box padding="3px" backgroundColor="D80">
+      <Button priority="secondary" size="small" suffixIcon={<Icons.ChevronDownSmall />}>
+        Suffix
+      </Button>
+    </Box>  
+  </Layout>
+  <Layout cols={4} gap="10px" alignItems="center">
+    <Box padding="3px" backgroundColor="D80">
+      <Button size="small" disabled prefixIcon={<Icons.ChevronDownSmall />}>
+        Prefix
+      </Button>
+    </Box>  
+    <Box padding="3px" backgroundColor="D80">
+      <Button priority="secondary" size="small" disabled prefixIcon={<Icons.ChevronDownSmall />}>
+        Prefix
+      </Button>
+    </Box>
+     <Box padding="3px" backgroundColor="D80">
+      <Button size="small" disabled suffixIcon={<Icons.ChevronDownSmall />}>
+        Suffix
+      </Button>
+    </Box>  
+    <Box padding="3px" backgroundColor="D80">
+      <Button priority="secondary" disabled size="small" suffixIcon={<Icons.ChevronDownSmall />}>
+        Suffix
+      </Button>
+    </Box>  
   </Layout>
 </ThemeProvider>
 </div>
