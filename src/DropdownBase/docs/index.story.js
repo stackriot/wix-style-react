319a159699cbc838cc09ad4d48bd7009b048eb40
Egozi <egozi@wix.com>
2020-11-11 12:03:32 +0200

    <DropdownBase/> - Remove unused Readme file

--- src/DropdownBase/docs/index.story.js
+++ src/DropdownBase/docs/index.story.js
@@ -3,7 +3,6 @@ import {
   header,
   tabs,
   tab,
-  columns,
   description,
   importExample,
   example as baseExample,
@@ -188,13 +187,18 @@ export default {
       tab({
         title: 'Description',
         sections: [
-          description({
-            title: 'Description',
-            text: `There is a common behaviour of "Dropdown-like" components in our library. They have a trigger
-                element, and a list of items that opens when the user interacts with that trigger element. The
-                <DropdownBase/> component is a higher-level component that encapsulates that logic, and allows
-                you to create "Dropdown-like" component with ease.`,
-          }),
+          description(`
+A common dropdown mechanism using the Popover component.
+
+There is a common behaviour of "Dropdown-like" components in our library. They have a trigger
+element, and a list of items that opens when the user interacts with that trigger element. The
+\`<DropdownBase/>\` component is a higher-level component that encapsulates that logic, and allows
+you to create "Dropdown-like" component with ease.
+
+It uses the \`<DropdownLayout/>\` component to show the list of items, and the \`<Popover/>\`
+component under the hood in order to position the list in the best way possible. It also manage
+keyboard navigation out of the box.
+`),
 
           importExample(),
 
