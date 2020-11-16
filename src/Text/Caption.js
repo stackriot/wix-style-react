c9b6261e4c990f6e0ddd7ad49391ea4d852989a0
Shlomi Toussia-Cohen <shlomitc@wix.com>
2020-11-16 18:08:35 +0200

    <Caption/> - (internal refactor) - use design tokens instead of Text component (#6250)

--- src/Text/Caption.js
+++ src/Text/Caption.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import RawText from './RawText';
+import PropTypes from 'prop-types';
 import { st, classes } from './Caption.st.css';
 import Ellipsis, { extractEllipsisProps } from '../common/Ellipsis';
 import { EllipsisCommonProps } from '../common/PropTypes/EllipsisCommon';
 
-const CaptionWithEllipsis = ({ className, caption, ...props }) => {
+const CaptionWithEllipsis = ({
+  dataHook,
+  className,
+  tagName,
+  caption,
+  light,
+  children,
+  ...props
+}) => {
   const { ellipsisProps, componentProps } = extractEllipsisProps(props);
 
   return (
     <Ellipsis
       {...ellipsisProps}
       wrapperClassName={classes[caption]}
-      render={({ ref, ellipsisClasses }) => (
-        <RawText
-          {...componentProps}
-          size="medium"
-          weight="bold"
-          ref={ref}
-          className={st(classes.root, { caption }, ellipsisClasses(className))}
-        />
-      )}
+      render={({ ref, ellipsisClasses }) => {
+        return React.createElement(
+          tagName,
+          {
+            ref,
+            className: st(
+              classes.root,
+              { caption },
+              ellipsisClasses(className),
+            ),
+            'data-hook': dataHook,
+            'data-light': light,
+            ...componentProps,
+          },
+          children,
+        );
+      }}
     />
   );
 };
@@ -27,12 +43,26 @@ const CaptionWithEllipsis = ({ className, caption, ...props }) => {
 CaptionWithEllipsis.displayName = 'Caption';
 
 CaptionWithEllipsis.propTypes = {
-  ...RawText.propTypes,
+  /** Applied as data-hook HTML attribute that can be used in the tests */
+  dataHook: PropTypes.string,
+
+  /** class to be applied to the root element */
+  className: PropTypes.string,
+
+  /** tag name that will be rendered */
+  tagName: PropTypes.string,
+
+  caption: PropTypes.oneOf(['c1']),
+
+  /** make the text color lighter */
+  light: PropTypes.bool,
+
   ...EllipsisCommonProps,
 };
 
 CaptionWithEllipsis.defaultProps = {
-  ...RawText.defaultProps,
+  caption: 'c1',
+  tagName: 'span',
   ...Ellipsis.defaultProps,
 };
 
