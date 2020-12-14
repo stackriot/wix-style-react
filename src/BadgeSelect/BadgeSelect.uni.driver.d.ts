67e0cefc029356875711fabaf24d0756730eb86f
Alex Kubica <me@alexkubica.com>
2020-12-14 15:23:23 +0200

    add missing method types to BadgeSelectUniDriver (#6388)

--- src/BadgeSelect/BadgeSelect.uni.driver.d.ts
+++ src/BadgeSelect/BadgeSelect.uni.driver.d.ts
@@ -1,5 +1,13 @@
 import { BaseUniDriver } from 'wix-ui-test-utils/unidriver';
+import { BadgeSize, BadgeType, BadgeSkin } from '../Badge';
 
 export interface BadgeSelectUniDriver extends BaseUniDriver {
+  getContent: () => Promise<HTMLElement>;
+  text: () => Promise<string>;
+  getType: () => Promise<BadgeType>;
+  getSkin: () => Promise<BadgeSkin>;
+  getSize: () => Promise<BadgeSize>;
+  isUppercase: () => Promise<boolean>;
+  hasClickCursor: () => Promise<boolean>;
   clickAtOption: (index: number) => Promise<void>;
 }
