7f8a6d48916dff5d0eaf66e23de052404a176570
Adi Cicurel <48910362+adicicurel@users.noreply.github.com>
2021-01-24 11:00:17 +0200

    Themes | Business Dashboard - LinearProgressBar error state (#6622)

--- packages/wix-style-react/src/Themes/businessDashboard/docs/components/LinearProgressBar.js
+++ packages/wix-style-react/src/Themes/businessDashboard/docs/components/LinearProgressBar.js
@@ -2,5 +2,6 @@ export const playground = `
 <ThemeProvider theme={theme()}>
   <LinearProgressBar skin="standard" value={30}/>
   <LinearProgressBar skin="success" value={30}/>
+  <LinearProgressBar error value={30}/>
 </ThemeProvider>
 `;
