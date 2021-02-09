faa2559c19abeb437664b049b4d729b85e61e0b8
Jonathan Adler <jonathana@wix.com>
2021-02-09 17:22:33 +0200

    ci: Adjust wix-ui-tpa release script

--- packages/wix-ui-tpa/scripts/tag.js
+++ packages/wix-ui-tpa/scripts/tag.js
@@ -1,11 +1,13 @@
 const { execute } = require('./utils');
 const logger = require('./logger');
 
+const TAG_PREFIX = 'WUT-v'
+
 function run() {
   if (process.env.IS_BUILD_AGENT) {
     try {
       const version = process.env.npm_package_version;
-      const tagName = `${version.startsWith('v') ? '' : 'v'}${version}`;
+      const tagName = `${TAG_PREFIX}${version}`;
 
       execute(`git tag -a ${tagName} -m "wix-ui-tpa version ${tagName}"`, true);
       execute(`git push origin ${tagName}`, true);
