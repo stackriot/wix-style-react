672ecd9232030b0bf3ffdf2b571d7ca794c7d2da
Jonathan Adler <jonathana@wix.com>
2021-02-27 20:13:29 +0200

    test(perfer): fix some perfer tests and their bundle config

--- packages/wix-ui-tpa/src/components/Popover/perf/PopoverBasic.tsx
+++ packages/wix-ui-tpa/src/components/Popover/perf/PopoverBasic.tsx
@@ -1,14 +1,9 @@
 import * as React from 'react';
 import { Popover, TriggerAction } from '../';
-import { classes } from './PopoverExtended.st.css';
 
 export const PopoverPerf = () => {
   return (
-    <Popover
-      placement={'top'}
-      className={classes.root}
-      triggerAction={TriggerAction.hover}
-    >
+    <Popover placement={'top'} triggerAction={TriggerAction.hover}>
       <Popover.Element>This is the Popover.Element</Popover.Element>
       <Popover.Content>This is the Popover.Content</Popover.Content>
     </Popover>
