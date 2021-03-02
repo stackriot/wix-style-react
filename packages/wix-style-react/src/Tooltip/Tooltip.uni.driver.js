51f37156e5fd90e60f11e6ad979aa8293a0cb7f5
roibWix <36195609+roibWix@users.noreply.github.com>
2021-03-02 16:48:57 +0200

    <Tooltip/> - fix testkit for puppeteer (#6859)

--- packages/wix-style-react/src/Tooltip/Tooltip.uni.driver.js
+++ packages/wix-style-react/src/Tooltip/Tooltip.uni.driver.js
@@ -1,5 +1,5 @@
 import { tooltipDriverFactory as tooltipDriverFactoryCore } from 'wix-ui-core/dist/src/components/tooltip/Tooltip.uni.driver';
-import { baseUniDriverFactory } from 'wix-ui-test-utils/base-driver';
+import { baseUniDriverFactory } from '../../test/utils/unidriver';
 
 export const tooltipDriverFactory = (base, body) => {
   const coreTooltipDriver = tooltipDriverFactoryCore(base, body);
@@ -13,16 +13,82 @@ export const tooltipDriverFactory = (base, body) => {
     tooltipExists: coreTooltipDriver.tooltipExists,
 
     /** mouse over the target element */
-    mouseEnter: coreTooltipDriver.mouseEnter,
+    mouseEnter: async () => {
+      switch (base.type) {
+        case 'react':
+          return coreTooltipDriver.mouseEnter();
+
+        case 'puppeteer':
+          return base.hover();
+        default:
+          return;
+      }
+    },
 
     /** mouse leaves the target element */
-    mouseLeave: coreTooltipDriver.mouseLeave,
+    mouseLeave: async () => {
+      switch (base.type) {
+        case 'react':
+          return coreTooltipDriver.mouseLeave();
+
+        case 'puppeteer':
+          const { element } = await baseUniDriverFactory(base).element();
+          await page.evaluate(async element => {
+            const event = new MouseEvent('mouseout', {
+              bubbles: true,
+              view: window,
+              cancelable: true,
+            });
+            await element.dispatchEvent(event);
+          }, element);
+          await page.waitFor(200);
+          return;
+
+        default:
+          return;
+      }
+    },
 
     /** returns tooltips content value in string */
-    getTooltipText: coreTooltipDriver.getTooltipText,
+    getTooltipText: async () => {
+      switch (base.type) {
+        case 'react':
+          return coreTooltipDriver.getTooltipText();
 
+        case 'puppeteer':
+          await base.hover();
+          const contentHook = await base.attr('data-content-hook');
+          const contentSelector = `[data-content-element="${contentHook}"]`;
+          const element = await body.$(contentSelector);
+          const text = await element.text();
+          await tooltipDriverFactory(base, body).mouseLeave();
+          return text;
+
+        default:
+          return;
+      }
+    },
     /** Clicks outside the tooltip element in order to dismiss it */
-    clickOutside: coreTooltipDriver.clickOutside,
+    clickOutside: async () => {
+      switch (base.type) {
+        case 'react':
+          return coreTooltipDriver.clickOutside();
+
+        case 'puppeteer':
+          await page.evaluate(() => {
+            document
+              .querySelector('body')
+              .dispatchEvent(new MouseEvent('mousedown'));
+            document
+              .querySelector('body')
+              .dispatchEvent(new MouseEvent('mouseup'));
+          });
+          return;
+
+        default:
+          return;
+      }
+    },
 
     /**
      * Checks whether tooltip has a given size
