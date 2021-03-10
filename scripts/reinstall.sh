dd32fe7a8deff4bac512d9f27db7e8ffa0d2e1b2
Arijus Šukys <arijuss@wix.com>
2021-03-10 13:16:41 +0200

    update reinstall script to not use lerna (#6906)
    although bash scripts should be kept to a minimum, here we don't really
    have a choice. `npm run reinstall` should work no matter if
    `node_modules` are available or not, it should working in any kind of
    state of the monorepo.
    
    thus, we cannot use `lerna exec -- rm -rf ...`, which simplifies
    removing of `dist` and similar. That's because user might try to `npm
    run reinstall` when node_modules are corrupted, not installed or half
    installed and `lerna` is not available.

--- scripts/reinstall.sh
+++ scripts/reinstall.sh
@@ -5,14 +5,17 @@
 # For example, after some infra changes it might be required to reinstall `node_modules`
 # It should be enough to run only `npm run reinstall` and all the heavy lifting is done behind the scenes.
 
-# Remove node_modules from all packages
-lerna exec -- rm -rf node_modules
+rm -rf node_modules package-lock.json
 
-# Remove package-lock.json from all packages
-lerna exec -- rm -f package-lock.json
-
-# Remove `dist` folder from all packages
-lerna exec -- rm -rf dist
+# Remove node_modules, package-lock.json, yarn.lock and dist from all packages
+# we can't use `lerna exec` here, because during reinstall `node_modules` might not be available
+find packages \( \
+  -name node_modules -or \
+  -name package-lock.json -or \
+  -name yarn.lock -or \
+  -name dist \
+  \) \
+  -exec rm -rf {} \;
 
 npm install
 # postinstall should run automatically after `npm install`
