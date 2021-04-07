8051720e1342c83f8318377b7ad564d7b978faf2
Shelly Goldblit <69430383+shelly-goldblit@users.noreply.github.com>
2021-04-07 11:31:59 +0300

    BusinessDashboard Theme | <Thumbnail> - Apply overrides to all size thumbnail (#6992)
    * apply overrides to all size thumbnail
    
    * Update Thumbnail.js
    
    * fix typo
    
    * Update Thumbnail.js

--- packages/wix-style-react/src/Themes/businessDashboard/docs/components/Thumbnail.js
+++ packages/wix-style-react/src/Themes/businessDashboard/docs/components/Thumbnail.js
@@ -2,13 +2,31 @@ export const playground = `
   <ThemeProvider theme={theme()}>
     <Layout cols={2}>
       <Cell span={2}>
-        <Thumbnail size="small" title="Thumbnail 1" />
+        <Thumbnail size="small" title="Small Thumbnail 1" />
       </Cell>
       <Cell span={2}>
-        <Thumbnail size="small" selected title="Selecetd 2" />
+        <Thumbnail size="small" selected title="Small Selected 2" />
       </Cell>
       <Cell span={2}>
-        <Thumbnail size="small" disabled title="Disabled 3" />
+        <Thumbnail size="small" disabled title="Small Disabled 3" />
+      </Cell>
+      <Cell span={2}>
+        <Thumbnail title="Thumbnail 4" />
+      </Cell>
+      <Cell span={2}>
+        <Thumbnail selected title="Selected 5" />
+      </Cell>
+      <Cell span={2}>
+        <Thumbnail disabled title="Disabled 6" />
+      </Cell>
+      <Cell span={2}>
+        <Thumbnail size="tiny" title="Tiny Thumbnail 7" />
+      </Cell>
+      <Cell span={2}>
+        <Thumbnail size="tiny" selected title="Tiny Selected 8" />
+      </Cell>
+      <Cell span={2}>
+        <Thumbnail size="tiny" disabled title="Tiny Disabled 9" />
       </Cell>
     </Layout>
   </ThemeProvider>
